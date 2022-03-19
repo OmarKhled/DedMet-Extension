fa912d081e6f80fbf43e2c4fbd42f439df1a6ca1
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-03-19 02:07:15 +0200

    Extracting timeline successfully

--- bgp.js
+++ bgp.js
@@ -1,47 +1,67 @@
-let c = 1;
-chrome.webRequest.onBeforeRequest.addListener(
-  async (details) => {
-    if (details.url.includes("core_calendar_get_calendar_monthly_view")) {
-      console.log(details.url);
-      const sesskey = details.url.split("=")[1].split("&")[0];
-      setTimeout(() => {
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            chrome.tabs.sendMessage(
-              tabs[0].id,
-              {
-                url: sesskey,
-              },
-              (res) => {
-                console.log(res);
-                fetch(
-                  `https://courses.nu.edu.eg/lib/ajax/service.php?sesskey=${sesskey}&info=core_calendar_get_calendar_monthly_view`,
-                  {
-                    headers: {
-                      accept: "application/json, text/javascript, */*; q=0.01",
-                      "content-type": "application/json",
-                      Cookie: `MoodleSession=${res.cookie}`,
-                      Connection: "keep-alive",
-                    },
-                    referrer: "https://courses.nu.edu.eg/my/index.php",
-                    referrerPolicy: "strict-origin-when-cross-origin",
-                    body: '[{"index":0,"methodname":"core_course_get_recent_courses","args":{"userid":"4174","limit":10}}]',
-                    method: "POST",
-                    mode: "cors",
-                    credentials: "include",
-                  }
-                )
-                  .then((response) => response.json())
-                  .then((response) => console.log(response))
-                  .catch((error) => console.log("Error:", error));
-              }
-            );
-            c += 1;
+const listener = (details) => {
+  if (details.url.includes("core_calendar_get_calendar_monthly_view")) {
+    const sesskey = details.url.split("=")[1].split("&")[0];
+    console.log(details.url);
+    setTimeout(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          {
+            url: sesskey,
+            type: "sesskey",
+          },
+          (res) => {
+            console.log(res);
+            fetchTimeLine(sesskey, res.cookie);
           }
         );
       });
+    }, 200);
+  }
+};
+
+const fetchTimeLine = (sesskey, cookie) => {
+  chrome.webRequest.onBeforeRequest.removeListener(listener);
+  fetch(
+    `https://courses.nu.edu.eg/lib/ajax/service.php?sesskey=${sesskey}&info=core_calendar_get_calendar_monthly_view`,
+    {
+      headers: {
+        accept: "application/json, text/javascript, */*; q=0.01",
+        "content-type": "application/json",
+        Cookie: `MoodleSession=${cookie}`,
+        Connection: "keep-alive",
+      },
+      referrer: "https://courses.nu.edu.eg/my/index.php",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: '[{"index":0,"methodname":"core_calendar_get_calendar_monthly_view","args":{"year":2022,"month":3,"courseid":1,"categoryid":0,"includenavigation":true,"mini":true}}]',
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
     }
-  },
-  { urls: ["<all_urls>"] }
-);
+  ).then((response) => {
+    response
+      .json()
+      .then((res) => {
+        console.log(res);
+        setTimeout(() => {
+          chrome.webRequest.onBeforeRequest.addListener(listener, {
+            urls: ["<all_urls>"],
+          });
+        }, 200);
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              timeline: res,
+              type: "res",
+            });
+          }
+        );
+      })
+      .catch((error) => console.log("Error:", error));
+  });
+};
+
+chrome.webRequest.onBeforeRequest.addListener(listener, {
+  urls: ["<all_urls>"],
+});
--- content.js
+++ content.js
@@ -1,8 +1,16 @@
+// window.addEventListener("load", (event) => {
+//   chrome.runtime.sendMessage({ loaded: true });
+// });
 chrome.runtime.onMessage.addListener((msg, sen, send) => {
-  const cookie = document.cookie.split("=")[1];
-  console.log(msg.url);
-  console.log(`Session ID: ${msg.url} & Cookie : ${cookie}`);
-  send({ cookie });
+  if (msg.type == "sesskey") {
+    const cookie = document.cookie.split("=")[1];
+    console.log(msg.url);
+    console.log(`Session ID: ${msg.url} & Cookie : ${cookie}`);
+    send({ cookie });
+  } else {
+    console.log(msg.timeline);
+  }
+
   return true;
 });
 console.log(document.cookie);
