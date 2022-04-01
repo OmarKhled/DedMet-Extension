82a44e14ea44014b41bf26614e301f11189cbec5
omar <o.abuelfetohahmed@nu.edu.eg>
2022-04-01 17:17:39 +0200

    Icons Change

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   devtool: false,
   entry: {
     bg: "./src/bg.ts",
